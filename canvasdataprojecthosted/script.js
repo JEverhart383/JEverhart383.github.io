5622a88815203902d47543799a5a34533ce415e1
JEverhart383 <jeffeverhart383@gmail.com>
2016-03-16 13:37:31 -0400

    delete for loop

--- canvasdataprojecthosted/script.js
+++ canvasdataprojecthosted/script.js
@@ -215,35 +215,6 @@ function categoryOverview(catDataPath){
 	var svg = dimple.newSvg("#chart3", 1400, 500);
     d3.json(catDataPath, function (data) { 	
 
-    	for (var i = 0; i < data.length; i++){
-
-    		if(data[i].term_name === "Summer I 2013"){
-    			data[i].order = "a"; 
-    		} else if (data[i].term_name === "Summer II 2013"){
-    			data[i].order = "b"; 
-    		} else if (data[i].term_name === "Fall 2013"){
-    			data[i].order = "c"; 
-    		} else if (data[i].term_name === "Spring 2014"){
-    			data[i].order = "d"; 
-    		} else if (data[i].term_name === "Summer I 2014"){
-    			data[i].order = "e"; 
-    		} else if (data[i].term_name === "Summer II 2014"){
-    			data[i].order = "f"; 
-    		} else if (data[i].term_name === "Fall 2014"){
-    			data[i].order = "g"; 
-    		} else if (data[i].term_name === "Spring 2015"){
-    			data[i].order = "h"; 
-    		} else if (data[i].term_name === "Summer I 2015"){
-    			data[i].order = "i"; 
-    		} else if (data[i].term_name === "Summer II 2015"){
-    			data[i].order = "j"; 
-    		} else if (data[i].term_name === "Fall 2015"){
-    			data[i].order = "k"; 
-    		}
-
-    	}
-
-    	console.log(data); 
 
       var myChart = new dimple.chart(svg, data); 
       myChart.setBounds(65, 30, 1200, 450)
